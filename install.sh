f0619b71fc421ec4bd98f39ef53e6ffed46fcd01
Eric Stewart <ericleestewart@gmail.com>
2017-01-17 23:08:12 -0500

    Move executables to bin folder.

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+# All the bin files to be installed.
+FILES=./bin/*
+
 # Create ~/bin directory if it doesn't exist.
 if [ ! -d "$HOME/bin" ]; then
   echo "Creating $HOME/bin."
   mkdir -p "$HOME/bin"
 fi
 
-# Copy scripts.
-echo "Install tools."
-cp easy-pass $HOME/bin
+# Install all the files in bin to user's bin folder.
+echo "Installing tools..."
+for f in $FILES
+do
+  cp "$f" "$HOME/bin/"
+done
